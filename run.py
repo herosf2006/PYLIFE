fef8cd372ebd3a369d6b0bbbb014d51c10ee0032
Bùi Tuấn Anh <herosf2006@yahoo.com>
2018-07-18 07:14:57 +0700

    add python files

---
+++ run.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+""" This script prompts a user to enter a message to encode or decode
+    using a classic Caeser shift substitution (3 letter shift)"""
+
+from app import app
+
+app.run(debug=True)
